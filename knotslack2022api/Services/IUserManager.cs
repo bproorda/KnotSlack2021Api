712d3617532dccccfa32c4d6e003ce61242403bd
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2022-02-07 11:17:16 -0600

    scaffolded out usermanger and its interface

---
+++ knotslack2022api/Services/IUserManager.cs
@@ -0,0 +1,25 @@
+﻿using knotslack2022api.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace knotslack2022api.Services
+{
+    public interface IUserManager
+    {
+        Task<KSUser> FindByNameAsync(string username);
+        Task<bool> CheckPasswordAsync(KSUser user, string password);
+        Task AccessFailedAsync(KSUser user);
+        Task<IdentityResult> CreateAsync(KSUser user, string password, string role);
+        Task<bool> AdminCheck();
+        Task<bool> IsUserAdmin(KSUser user);
+        Task<IList<string>> GetUserRoles(KSUser user);
+        Task<KSUser> FindByIdAsync(string userId);
+        Task<KSUser> FindAllLoggedInUsers();
+        //Task<IdentityResult> UpdateAsync(KSUser user);
+        //Task<IdentityResult> DeleteUser(string username);
+        //Task<UserWithToken> CreateUserWithToken(KSUser user);
+        //Task<UserWithoutToken> CreateUserWithoutToken(KSUser user);
+        //Task<List<createChannelDTO>> GetUserChannels(KSUser user);
+        //Task<UserChannel> AddNewUserToGeneral(string username);
+        Task<string> CreateToken(KSUser user);
+    }
+}
---
+++ knotslack2022api/Services/UserManager.cs
@@ -0,0 +1,58 @@
+﻿using knotslack2022api.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace knotslack2022api.Services
+{
+    public class UserManager : IUserManager
+    {
+        public Task AccessFailedAsync(KSUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AdminCheck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> CheckPasswordAsync(KSUser user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> CreateAsync(KSUser user, string password, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> CreateToken(KSUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<KSUser> FindAllLoggedInUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<KSUser> FindByIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<KSUser> FindByNameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> GetUserRoles(KSUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUserAdmin(KSUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
